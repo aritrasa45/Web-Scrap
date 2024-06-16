983f4963502ba459c86bf200c42ea319c6707dcc
Aritraso <143035320+aritrasa45@users.noreply.github.com>
2024-06-17 00:03:58 +0530

    main.py

---
+++ Tool/main.py
@@ -0,0 +1,76 @@
+# by aritrasa
+
+import time , os , sys
+
+try:
+	import requests
+
+except:
+	os.system('pip3 install requests')
+
+			
+
+
+os.system('clear')
+
+DARKCYAN = '\033[36m'
+BLUE = '\033[94m'
+GREEN = '\033[92m'
+RED = '\033[91m'
+BOLD = '\033[1m'
+END = '\033[0m'
+
+while True:
+	link = input(f"{GREEN}• The link [ :  {END}") 
+	if  'https://' in link or 'http://' in link :
+		print(f"{DARKCYAN}Processing...{END}")
+		break
+		
+		
+	else:
+		os.system('clear')
+		print(f"{BOLD}{RED}Start with\n[http://]     or        [https://]{END}")
+		continue
+
+
+x = f'{link}'
+try:
+	data = requests.get(x)
+	html = data.text
+	
+except requests.exceptions.InvalidURL as e:
+    print(f"{RED}! Invalid URL error: {e}{END}")
+    sys.exit()
+except requests.exceptions.HTTPError as e:
+    print(f"{RED}! HTTP error: {e}{END}")		
+    sys.exit()
+
+while True:
+	cont = input(f"\n{GREEN}Do you wanna continue [Y/n] : {END}")
+	
+	if cont =='y' or cont =='Y' or cont=='':
+		break
+		
+	elif cont =='n' or cont =='N':
+		sys.exit()
+			
+	else:
+		print(f"{BOLD}{RED}! Wrong input{END}")
+		os.system('clear')
+	continue
+	
+	
+	
+name1 = input(f"{GREEN}¥ filename  [: {END}")
+
+
+with open (f"{name1}.html", "a") as file:
+	file.write(f"{html}")
+	file.close()
+	
+	
+time.sleep(2)
+		
+os.system('clear')
+print(f"{DARKCYAN}▪︎ ¥ [{name1}.html] saved in storage {END}")
+
